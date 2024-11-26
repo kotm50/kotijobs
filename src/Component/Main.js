de169906c6602ecd7140ae537d546dcb608be7b3
kotm50 <koreatm50@gmail.com>
2024-11-26 11:18:40 +0900

    백업

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,17 +1,21 @@
 import React from "react";
 
 import Jumbotron from "./Main/Jumbotron";
+/* 백업
 import GoldJobs from "./Main/GoldJobs";
 import PlatinumJobs from "./Main/PlatinumJobs";
+
+      <div className="grid grid-cols-1 gap-y-4">
+        <PlatinumJobs />
+        <GoldJobs />
+      </div>
+
+      */
 
 function Main() {
   return (
     <>
       <Jumbotron />
-      <div className="grid grid-cols-1 gap-y-4">
-        <PlatinumJobs />
-        <GoldJobs />
-      </div>
     </>
   );
 }
