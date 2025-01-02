da77c48307aef92c94e972cbce2ef9e295beb138
kotm50 <koreatm50@gmail.com>
2025-01-02 13:51:10 +0900

    로그인관리

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import AdminMain from "./Pages/AdminMain";
 import Header from "./Components/Header";
 import Login from "./Pages/Login";
 import AdInput from "./Pages/AdInput";
 import AdList from "./Pages/AdList";
 import Test from "./Pages/Test";
+import { clearUser } from "./Reducer/userSlice";
 
 function App() {
   const thisLocation = useLocation();
+  const login = useSelector(state => state.user);
+  const navi = useNavigate();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    console.log(login);
+    if (login.userId) {
+      console.log(login);
+      navi("/admin");
+    } else {
+      dispatch(clearUser());
+      navi("/");
+    }
+    //eslint-disable-next-line
+  }, [thisLocation]);
   return (
     <>
       <Header thisLocation={thisLocation} />
